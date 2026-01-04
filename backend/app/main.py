465e5795f8e3057a60885853b3c2f133756116b1
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2026-01-05 03:36:23 +0500

    add cors to backend to allow requests from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routers import auth 
 from app import models
 
 app = FastAPI(title="Klaviyo Nexus Backend")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)
